8a731d5c62c4a506e450f9c718aae2ee14bc8906
Dafin <dafin@users.noreply.github.com>
2016-05-05 18:41:05 +0100

    Building up Location class, including the routing

--- views/location.rb
+++ views/location.rb
@@ -1,1 +1,36 @@
-location.rb
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+
+class Location
+    include DataMapper::Resource
+
+    property :id, Serial
+# unlike , AR, which gives id automatically 
+    property :name, String
+    property :lat, Float
+    property :long, Float
+    property :blurb, Text
+    
+end
+
+DataMapper.finalize
+
+
+
+get '/locations' do
+	@location = Location.all
+	slim :locations	
+end 
+
+
+get '/locations/new' do
+	@location = Location.new
+	slim :new_location	
+end 
+
+get '/edit' do
+	...
+
+end
